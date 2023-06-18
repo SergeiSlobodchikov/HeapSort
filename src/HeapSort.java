dc44a0abc333b3f1d194401130e641b962eefb8e
Sergei Slobodchikov <cerhclobodhikow@mail.ru>
2023-06-18 19:56:19 +0300

    Домашнее задание

---
+++ src/HeapSort.java
@@ -0,0 +1,14 @@
+package src;
+
+public class HeapSort {
+    public static void heapSort(int[] array) {
+        int n = array.length;
+
+        Heap.buildHeap(array, n);
+
+        for (int i = n - 1; i > 0; i--) {
+            Heap.swap(array, 0, i);
+            Heap.heapify(array, i, 0);
+        }
+    }
+}
